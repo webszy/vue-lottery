cf4e0e7019884e93b02c5ecffc2e37aaa899e2d5
孙振宇 <sunzhenyu@live.com>
2020-11-20 20:36:30 +0800

    inital commit

--- README.md
+++ README.md
@@ -1,49 +1,19 @@
-# vue-lottery
+# lottery
 
-在线演示地址: [https://venler.github.io/demo/vue-lottery/index.html](https://venler.github.io/demo/vue-lottery/index.html)
-
-## How To Use
-
-> ES6
-
-```js
-npm install vue-lottery --save
+## Project setup
+```
+npm install
 ```
 
+### Compiles and hot-reloads for development
 ```
-import lottery from 'vue-lottery'
-Vue.use(lottery)
-```
-
-## 调用示例
-
-```
-<lottery
-    @lotteryClick="lotteryClick"
-    @lotteryDone="lotteryDone"
-    :lottery-start="0"
-    :lottery-prizenum="8"
-    :lottery-prizeno="1"
-    lottery-bg="https://venler.github.io/demo/vue-lottery/dist/turnplate-bg.png"
-    content-bg="https://venler.github.io/demo/vue-lottery/dist/turntable.png"
-    pointer-bg="https://venler.github.io/demo/vue-lottery/dist/pointer.png"
-    :lottery-width="['85%','35%']"
-/>
+npm run serve
 ```
 
-### props说明
+### Compiles and minifies for production
+```
+npm run build
+```
 
-| 参数名 | 参数类型 | 解释 | 是否必传 |
-| :--- | :--- | :--- | :--- |
-| lottery-prizenum | Number | 奖品总数量 | 是 |
-| lottery-prizeno | Number | 从起点开始逆时针算，中奖的是第几个 | 是 |
-| lottery-bg | String | 背景图 | 否 |
-| content-bg | String | 内容区域背景图 | 是 |
-| pointer-bg | String | 指针背景图 | 是 |
-| lottery-start | Number | 0为停止，1为开始转动 | 否 |
-| lotteryDone | Function | 旋转完成后的回调函数 | 否 |
-| lotteryClick | Function | 点击抽奖按钮的回调 | 否 |
-| lottery-width | String | 内外圈间距百分比 | 否 |
-
-
-
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-    <div id="app">
-        <Index />
-    </div>
+  <div id="app">
+    <lottery
+      @lotteryClick="lotteryClick"
+      @lotteryDone="lotteryDone"
+      :lottery-start="lotteryStart"
+      :lottery-prizenum="8"
+      :lottery-prizeno="1"
+      lottery-bg="https://venler.github.io/demo/vue-lottery/static/turnplate-bg.png"
+      content-bg="https://venler.github.io/demo/vue-lottery/static/turntable.png"
+      pointer-bg="https://venler.github.io/demo/vue-lottery/static/pointer.png"
+      :lottery-width="['85%', '35%']"
+    />
+  </div>
 </template>
 
 <script>
-import Index from './components/Index'
+import Lottery from './components/lottery.vue'
 
 export default {
-    name: 'App',
-    components: {
-        Index
+  name: 'App',
+  components: {
+    Lottery
+  },
+  data(){
+    return{
+         lotteryStart: 0,
+            prizeNo: 1,
+            prizeNum: 8,
+            prizeList: ['200万里通积分', '288元万里通积分红包', '50万里通积分', '10万里通积分', '感谢参与', '88元万里通积分红包', '100万里通积分', '888元万里通积分红包']
     }
+  },
+  methods:{
+     lotteryClick () {
+            this.lotteryStart = 1
+            let randomNum = 1 + parseInt(Math.random() * this.prizeNum)
+            this.prizeNo = randomNum
+        },
+        lotteryDone (res) {
+            this.lotteryStart = 0
+            let index = res.prizeNo - 1
+            console.log(this.prizeList[index])
+        }
+  }
 }
-
 </script>
 
 <style>
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
+#app {
+  width: 100%;
+  height: 100%;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
